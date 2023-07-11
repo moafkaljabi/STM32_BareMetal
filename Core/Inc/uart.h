d631df599dc283ed4a26ac671931a43e33b90a68
moafk <62897604+moafkaljabi@users.noreply.github.com>
2023-07-11 18:48:37 +0300

    Add files via upload

---
+++ Core/Inc/uart.h
@@ -0,0 +1,14 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jul 11, 2023
+ *      Author: mwafa2
+ */
+
+#ifndef INC_UART_H_
+#define INC_UART_H_
+
+#include "uart.c"
+
+
+#endif /* INC_UART_H_ */
---
+++ Core/Src/uart.c
@@ -0,0 +1,8 @@
+/*
+ * uart.c
+ *
+ *  Created on: Jul 11, 2023
+ *      Author: mwafa2
+ */
+
+
